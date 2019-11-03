8e9456c2e417a43d5573d798e0f388b9d345829e
Satria Kemal <satria.kemal.08@gmail.com>
2019-11-03 10:23:11 +0700

    user data backend done

---
+++ admin/user_data-delete.php
@@ -0,0 +1,18 @@
+<?php
+
+    require_once "../database/db_login.php";
+
+    $id = $_GET['id'];
+    $query = "DELETE FROM mahasiswa WHERE NIM = {$id}";
+    $result = mysqli_query($db, $query);
+    if($result){
+        header("Location: user_data.php");
+    }else{
+        die($query);
+        mysqli_connect_errno();
+        
+    }
+
+
+
+?>
--- admin/user_data-interface.php
+++ admin/user_data-interface.php
@@ -2,7 +2,41 @@
 
 <?php
         require_once("../database/db_login.php");
-        $id = $_POST['input'];
+        if($_POST){
+            $id = $_POST['id'];
+        }
+        if($_GET){
+            $id = $_GET['id'];
+        }
+        
+        $query = "SELECT * FROM mahasiswa WHERE NIM = '{$id}'";
+        $result = mysqli_query($db, $query);
+        if(!$result){
+            die("Query Error");
+            mysqli_connect_errno();
+        }else{
+            if($_GET){
+                $data = mysqli_fetch_array($result);
+                $nama = $data['Nama'];
+                $alamat = $data['Alamat'];
+                $tempat = $data['tempat_lahir'];
+                $tanggal = $data['tanggal_lahir'];
+                $email = $data['email'];
+                $hp = $data['hp'];
+                $wali = $data['wali'];
+                $hp_wali = $data['hp_wali'];
+            }else{
+                $nama = '';
+                $alamat = '';
+                $tempat = '';
+                $tanggal = '';
+                $email = '';
+                $hp = '';
+                $wali = '';
+                $hp_wali = '';
+            }
+            
+        }
 ?>
         <div class="row">
             
@@ -18,49 +52,49 @@
                             <tr>
                                 <td>NAMA</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="text" name="nama" value=""></td>
+                                <td><input class="form-control" type="text" name="nama" value="<?php echo $nama; ?>"></td>
                             </tr>
                             <tr>
                                 <td>NIM</td>
                                 <td>:</td>
-                                <td><input name="NIM" class="form-control" type="text" value="<?php echo $id; ?>"></td>
+                                <td><input name="NIM" class="form-control" type="text" value="<?php echo $id; ?>" ></td>
 
                                 
                             </tr>
                             <tr>
                                 <td>Alamat</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="text" name="alamat" value=""></td>
+                                <td><input class="form-control" type="text" name="alamat" value="<?php echo $alamat; ?>"></td>
                             </tr>
                             <tr>
                                 <td>Tempat Lahir</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="text" name="tempat-lahir" value=""></td>
+                                <td><input class="form-control" type="text" name="tempat-lahir" value="<?php echo $tempat; ?>"></td>
                             </tr>
                             <tr>
                                 <td>Tanggal Lahir</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="date" name="tanggal-lahir" value=""></td>
+                                <td><input class="form-control" type="date" name="tanggal-lahir" value="<?php echo $tanggal; ?>"></td>
                             </tr>
                             <tr>
                                 <td>HP</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="text" name="hp" value=""></td>
+                                <td><input class="form-control" type="text" name="hp" value="<?php echo $hp; ?>"></td>
                             </tr>
                             <tr>
                                 <td>email</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="email" name="email" value=""></td>
+                                <td><input class="form-control" type="email" name="email" value="<?php echo $email; ?>"></td>
                             </tr>
                             <tr>
                                 <td>Nama Wali</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="text" name="wali" value=""></td>
+                                <td><input class="form-control" type="text" name="wali" value="<?php echo $wali; ?>"></td>
                             </tr>
                             <tr>
                                 <td>HP Wali</td>
                                 <td>:</td>
-                                <td><input class="form-control" type="text" name="wali-hp" value=""></td>
+                                <td><input class="form-control" type="text" name="wali-hp" value="<?php echo $hp_wali; ?>"></td>
                             </tr>
                             <tr>
                                 <td colspan=3 align="right"><button name="submit" id="" class="btn btn-primary" type="submit">Simpan</button></td>
