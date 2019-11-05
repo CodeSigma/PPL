493ff751172bd629549a4d801daa7440a6fc0ca1
obedreinhard <sobedreinhard@gmail.com>
2019-11-06 00:59:22 +0700

    fix login front-end

--- index.php
+++ index.php
@@ -1,61 +1,22 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
-
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/custom.css">
 </head>
 
 <body>
-    <div class="row">
-        <div class="col-xl-8 col-sm align-middle ">
-            <div class="bgimage"></div>
-            <div class="sia">
-                <h1 class="display-3">SISTEM INFORMASI AKADEMIK</h1>
-                <h1 class="display-4">UNIVERSITAS DIPONEGORO</h1>
-            </div>
-
-
+    <div class="wrapper">
+        <div class="left">
+            <h1>SISTEM INFORMASI AKADEMIK UNIVERSITAS DIPONEGORO</h1>
+            <!-- <img src="./images/bgleft.jpeg" alt="backgambar"> -->
         </div>
-
-
-        <div class="col-xl-4 col-sm-12 login">
+        <div class="right">
+            <img src="img/undip-logo.png" alt="logo">
             <form method="POST" action="login_session.php">
-                <table>
-                <tr>
-                    <td>Username:</td>
-                    <td><input class="form-control" type="text" name="username"></td>
-                </tr>
-                <tr>
-                    <td>Password:</td>
-                    <td><input class="form-control" type="password" name="password"></td>
-                </tr>
-                <tr>
-                    <td><button type="submit" class="btn btn-primary">Login</button></td>
-                </tr>   
-                </table>
-                
-                
+                <input type="text" name="username" placeholder="username">
+                <input type="password" name="password" placeholder="password">
+                <input type="submit" value="login">
             </form>
         </div>
     </div>
-
-
-
-
-
-
-    <script src="js/bootstrap.min.js" async defer></script>
 </body>
-
 </html>
