65884f1d284e374ccdbae89cdb16fcdbedb081b2
obedreinhard <sobedreinhard@gmail.com>
2019-11-12 08:09:36 +0700

    Edit Profil Frontend Done

---
+++ mahasiswa/headbar.php
@@ -0,0 +1,8 @@
+<div class="header">
+        <div class="menu">
+            <img src="images/menu.png" alt="menu">
+        </div>
+        <div class="logo">
+            <img src="images/logo.png" alt="logo">
+            <p>UNIVERSITAS DIPONEGORO</p>
+        </div>
