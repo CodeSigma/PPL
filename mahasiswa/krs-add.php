6fe16ce1787f9d3c9964bd433a697d8471ef2bb0
dindaayuaprillia <dindaayuaprillia@gmail.com>
2019-11-27 21:59:33 +0700

    validasi mahasiswa

--- mahasiswa/krs-add.php
+++ mahasiswa/krs-add.php
@@ -11,24 +11,53 @@
         header("Location: krs.php?action='edit'&alert='selected'.php");
     }else{
 
-        $query = "INSERT INTO krs
-        (NIM, Kode_Jadw, semester) Values ({$_SESSION['username']}, '{$_POST['matakuliah']}',{$row['semester']});
-        ";
-    $result = mysqli_query($db,$query);
-    print_r($query);
-    if(!$result){
-        ?>
-            <script>
-                document.addEventListener('DOMContentLoaded',function(){
-                    alert("Query gagal");
+        $cek1 = mysqli_query($db, "SELECT Kode_MK FROM jadwal WHERE Kode_Jadw = '{$_POST['matakuliah']}'  ");
+        $kodemk = mysqli_fetch_array($cek1);
+        $Kode_MK= $kodemk['Kode_MK'];
 
-                });
-            </script>
-        <?php
-            die(mysqli_error($db));
-        }else{
-            header("Location: krs.php?action='edit'.php");
+        $checkkodeMK = mysqli_query($db, "SELECT * FROM krs INNER JOIN jadwal ON krs.Kode_Jadw = jadwal.Kode_Jadw WHERE krs.NIM='{$_SESSION['username']}' AND jadwal.Kode_MK='{$Kode_MK}'");
+
+        if (mysqli_num_rows($checkkodeMK)!=0){
+            
+        header("Location: krs.php?action='edit'&alert='selected'.php");
+        } else {
+               $cek2 = mysqli_query($db, "SELECT Jam_Mulai, Hari FROM jadwal WHERE Kode_Jadw = '{$_POST['matakuliah']}'  ");
+                $jam = mysqli_fetch_array($cek2);
+                $jamMK= $jam['Jam_Mulai'];
+                $hariMK= $jam['Hari'];
+
+                $checkJam_Mulai = mysqli_query($db, "SELECT * FROM krs INNER JOIN jadwal ON krs.Kode_Jadw = jadwal.Kode_Jadw WHERE krs.NIM='{$_SESSION['username']}' AND jadwal.Jam_Mulai='{$jamMK}' AND jadwal.Hari='{$hariMK}' ");
+
+
+                if (mysqli_num_rows($checkJam_Mulai)!=0) {
+                    header("Location: krs.php?action='edit'&alert='selected'.php");
+                } else {
+                    # code...
+                    $query = "INSERT INTO krs (NIM, Kode_Jadw, semester) Values ({$_SESSION['username']}, '{$_POST['matakuliah']}',{$row['semester']})";
+
+                    $result = mysqli_query($db,$query);
+                    print_r($query);
+                    if(!$result){
+                    ?>
+                        <script>
+                            document.addEventListener('DOMContentLoaded',function(){
+                                alert("Query gagal");
+
+                            });
+                        </script>
+                    <?php
+                        die(mysqli_error($db));
+                    }else{
+                        header("Location: krs.php?action='edit'.php");
+                    }
+                }
+                
+            
+           
+           
+
         }
+        
     }
         
 
